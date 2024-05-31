79d02ee040c631c6fe15dff71a150195398a8de7
Jack White <jackwhite230@gmail.com>
2024-05-31 13:30:43 +0100

    basic app structure created

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,57 @@
-import AuthButton from "../components/AuthButton";
+import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { redirect } from "next/navigation";
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+export default async function Page() {
+  const supabase = createClient();
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+      <div className="w-full">
+        <nav className="w-full flex justify-center border-b h-16">
+          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+            <AuthButton />
+          </div>
+        </nav>
+      </div>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          some text here once logged in
         </main>
       </div>
 
-      <footer className="w-full border-t p-8 flex justify-center text-center text-xs">
+      <footer className="w-full border-t py-6 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
+          Created by Jack White <br />
+          <div className="flex flex-row justify-evenly">
+            <a
+              href="https://github.com/Chalky23"
+              target="_blank"
+              className="font-bold hover:underline"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://linkedin.com/in/white-jack"
+              target="_blank"
+              className="font-bold hover:underline"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
         </p>
       </footer>
     </div>
