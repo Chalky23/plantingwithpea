5df1e4d7e804c72274ce96c751df8ccc9452f7f9
Jack White <jackwhite230@gmail.com>
2024-05-31 13:00:17 +0100

    removed unused parts from template. renamed .env

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <nav className="w-full flex justify-center border-b h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
           <DeployButton />
           {isSupabaseConnected && <AuthButton />}
@@ -36,7 +36,7 @@ export default async function Index() {
         </main>
       </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+      <footer className="w-full border-t p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
           <a
